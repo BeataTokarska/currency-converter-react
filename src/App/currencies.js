31e02a29e1eb2d77a83a6f076a1a2ab27a270d8d
Beata Tokarska <tokarskabb@gmail.com>
2022-09-09 11:17:46 +0200

    currencies added

---
+++ src/App/currencies.js
@@ -0,0 +1,17 @@
+export const currencies = [
+    {
+        short: "USD",
+        name: "Dolar Amerykański",
+        rate: "4,3063",
+    },
+    {
+        short: "EUR",
+        name: "Euro",
+        rate: "4,6768",
+    },
+    {
+        short: "GBP",
+        name: "Funt Brytyjski",
+        rate: "5,6041",
+    },
+];
