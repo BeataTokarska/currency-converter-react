c00507a5af782d0f17399e40816760b966feebca
Beata Tokarska <tokarskabb@gmail.com>
2022-09-12 11:52:36 +0200

    format  text added

--- src/App/Form/index.js
+++ src/App/Form/index.js
@@ -17,7 +17,7 @@ export const Form = ({ calculateResult, result }) => {
             <fieldset className="converter__fieldset">
                 <legend className="converter__title">
                     Prosty przelicznik walut
-                    </legend>
+                </legend>
 
                 <p>
                     <label>
@@ -66,47 +66,11 @@ export const Form = ({ calculateResult, result }) => {
                 <button type="reset">Wyczyść</button>
             </p>
             <p>Otrzymana wartość: <strong>
-            <Result result={result} /></strong></p>
-            
+                <Result result={result} /></strong></p>
+
         </form >
     )
 };
 
-
-
-
-
-
-
-
-
-
-// <form className="converter">
-//     <fieldset className="converter__fieldset">
-//         <legend className="converter__title">Prosty przelicznik walut</legend>
-//         <p>
-//             <label>
-//                 Wybierz walutę:
-//                 <select name="currency">
-//                     <option value="USD">USD</option>
-//                     <option value="GBP" selected>GBP</option>
-//                     <option value="EUR">EUR</option>
-//                 </select>
-//             </label>
-//         </p>
-
-//         <p>
-//             <label>
-//                 wpisz kwotę w zł: <input  type="number" value="0.00" name="amountPLN" min="0.00"
-//                     step="0.01"/>
-//             </label>
-//         </p>
-//     </fieldset>
-//     <p className="converter__buttonContainer">
-//         <button>Przelicz</button>
-//         <button type="reset">Wyczyść</button>
-//     </p>
-//     <p>Otrzymana wartość: <strong ></strong></p>
-// </form>
 
 export default Form;
