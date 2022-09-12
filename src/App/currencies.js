5a28c3f8e16c291e0d5460baca6095f7706b7706
Beata Tokarska <tokarskabb@gmail.com>
2022-09-12 12:39:58 +0200

    rates format changed

--- src/App/currencies.js
+++ src/App/currencies.js
@@ -2,16 +2,16 @@ export const currencies = [
     {
         short: "USD",
         name: "Dolar Amerykański",
-        rate: "4,3063",
+        rate: "4.3063",
     },
     {
         short: "EUR",
         name: "Euro",
-        rate: "4,6768",
+        rate: "4.6768",
     },
     {
         short: "GBP",
         name: "Funt Brytyjski",
-        rate: "5,6041",
+        rate: "5.6041",
     },
 ];
