80ef597b6e12e20b0619014fd3b7018826137d1a
Beata Tokarska <tokarskabb@gmail.com>
2022-09-22 12:32:47 +0200

    clock added

--- src/App/index.js
+++ src/App/index.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Form } from "./Form";
 import { currencies } from "./currencies";
 import Rates from "./Rates";
+import { Clock } from "./Clock";
 
 function App() {
     const [result, setResult] = useState();
@@ -19,13 +20,17 @@ function App() {
     }
 
     return (
+        
         <body className="body">
+           
             <div className="app">
+              <Clock />   
                 <Form
                     result={result}
                     calculateResult={calculateResult}
                     setResult={setResult}
                 />
+                
             </div>
             <Rates />
             <p>*kurs walut na dzień 12.04.2022</p>
